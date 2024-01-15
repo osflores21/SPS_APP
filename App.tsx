0d0bf0a486c308be25495f6561038af2037fddcf
osflores21 <osflores67@gmail.com>
2024-01-14 23:21:33 -0600

    add status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import HomeStack from './src/navigation/HomeStack';
 import { I18nextProvider } from 'react-i18next';
@@ -8,10 +8,11 @@ import i18next from './src/functions/i18next';
 const App = () => {
   return (
     <I18nextProvider i18n={i18next}>
-    <NavigationContainer>
-    <HomeStack />
-  </NavigationContainer>
-  </I18nextProvider>
+      <NavigationContainer>
+        <StatusBar backgroundColor="#5555" />
+        <HomeStack />
+      </NavigationContainer>
+    </I18nextProvider>
   )
 }
 
